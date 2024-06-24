5a80e960c4946b259f22e5ad1cb980b501000981
Tarcísio Fudelao <tarcisiomenezes2019@hotmail.com>
2024-06-24 18:25:14 +0200

    Debugged Home/index.js

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,44 +1,44 @@
 import { useState, useEffect } from "react";
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import { styles } from "./styles";
-import { Input } from "@/components/input"
-import { Feather } from "@expo/vector-icons"
+import { Input } from "@/components/input";
+import { Feather } from "@expo/vector-icons";
 import { theme } from "@/theme";
 import { Contact } from "@/components/contact";
-import * as Contacts from "expo-contacts"
+import * as Contacts from "expo-contacts";
 
 export function Home() {
-    const [name, setName] = useState("")
+    const [name, setName] = useState("");
 
     async function fetchContacts() {
         try {
-            const { status } = await Contacts.requestPermissionsAsync()
+            const { status } = await Contacts.requestPermissionsAsync();
 
             if (status === Contacts.PermissionStatus.GRANTED) {
-                const { data } = await Contacts.getContactsAsync()
-                console.log(data)
+                const { data } = await Contacts.getContactsAsync();
+                console.log(data);
             }
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
 
     useEffect(() => {
-        fetchContacts()
-    }, [])
+        fetchContacts();
+    }, []);
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Input style={styles.input}>
                     <Feather name="search" size={16} color={theme.colors.gray_300} />
-                    <Input.Field placeholder="Search for name..."
-                    onChangeText={setName}
-                    value={name} />
+                    <Input.Field 
+                        placeholder="Search for name..."
+                        onChangeText={setName}
+                        value={name} 
+                    />
                     <TouchableOpacity onPress={() => setName("")}>
-                        <Feather name="x" size={16} 
-                        color={theme.colors.gray_300}
-                        />
+                        <Feather name="x" size={16} color={theme.colors.gray_300} />
                     </TouchableOpacity>
                 </Input>
             </View>
@@ -48,5 +48,5 @@ export function Home() {
                 image: require("@/assets/hero.png")
             }} />
         </View>
-    )
+    );
 }
--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -7,25 +7,29 @@ const variants = {
         large: { width: 100, height: 100, borderRadius: 32 },
     },
     text: {
-        medium: {fontSize: 24,},
-        large: { fontSize: 52,}
+        medium: { fontSize: 24 },
+        large: { fontSize: 52 }
     }
-}
+};
 
 type Props = {
-    name: string,
-    image?: ImageProps | null,
-    variant?: "medium" | "large"
-}
-export function Avatar ({ image, name, variant = "medium" }: Props) {
-    return <View> {
+    name: string;
+    image?: ImageProps | null;
+    variant?: "medium" | "large";
+};
 
-        image ? <Image source={image} style={variants.size[variant]} /> : 
-            <View style={[styles.letter, variants.size[variant]]}>
-                <Text style={[styles.text, variants.text[variant]]}>
-                    {name[0].toUpperCase()}
-                </Text>
-            </View>
-    }
-     </View>
+export function Avatar({ image, name, variant = "medium" }: Props) {
+    return (
+        <View>
+            {image ? (
+                <Image source={image} style={variants.size[variant]} />
+            ) : (
+                <View style={[styles.letter, variants.size[variant]]}>
+                    <Text style={[styles.text, variants.text[variant]]}>
+                        {name[0].toUpperCase()}
+                    </Text>
+                </View>
+            )}
+        </View>
+    );
 }
